c4afd3a1c5090dcf5641466f829cd0ca27de7334
xurob <>
2021-01-04 18:36:12 +0100

    added filteredreads stats

--- 02_merge_pileup_counts.sh
+++ 02_merge_pileup_counts.sh
@@ -2,11 +2,12 @@
 directory=$1
 samplename=$2
 directoryout=$3
-
+dir=$4
 # Combine all files
 cat ${directory}/*.A.txt | gzip > "${directoryout}/${samplename}_all.A.txt.gz"
 cat ${directory}/*.C.txt | gzip > "${directoryout}/${samplename}_all.C.txt.gz"
 cat ${directory}/*.G.txt | gzip > "${directoryout}/${samplename}_all.G.txt.gz"
 cat ${directory}/*.T.txt | gzip > "${directoryout}/${samplename}_all.T.txt.gz"
-cat ${directory}/*.coverage.txt | gzip > "${directoryout}/${samplename}_all.coverage.txt.gz"
-cat ${directory}/*.depth.txt | gzip > "${directoryout}/${samplename}_all.depthTable.txt.gz"
+cat ${dir}/*.filteredreadslog.txt | gzip > "${directoryout}/${samplename}_all.filteredreadslog.txt"
+cat ${directory}/*.coverage.txt | gzip > "${directoryout}/${samplename}_all.coverage.txt"
+cat ${directory}/*.depth.txt | gzip > "${directoryout}/${samplename}_all.depthTable.txt"
